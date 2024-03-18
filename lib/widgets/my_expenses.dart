d6f1f382532f3bbc113e80fdb57c7028efb15ab2
MSaJS <samara.sts.o@gmail.com>
2024-03-18 12:05:34 -0300

    criando appBar

--- lib/widgets/my_expenses.dart
+++ lib/widgets/my_expenses.dart
@@ -12,6 +12,14 @@ class MyExpenses extends StatefulWidget {
 }
 
 class _MyExpensesState extends State<MyExpenses> {
+  int selectedNavIndex = 0;
+  List<Icon> navBarIcons = [
+    Icon(Icons.home),
+    Icon(Icons.work),
+    Icon(Icons.calculate),
+    Icon(Icons.tab)
+  ];
+
   final List<Expense> _registeredExpenses = [
     Expense(
       title: 'Teste',
@@ -77,15 +85,17 @@ class _MyExpensesState extends State<MyExpenses> {
 
     return Scaffold(
       appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.arrow_back),
+            );
+          },
+        ),
+        title: Text('Minhas despesas',
+            style: Theme.of(context).textTheme.titleLarge),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.arrow_back),
-          ),
-          const SizedBox(width: 15),
-          Text('Minhas despesas',
-              style: Theme.of(context).textTheme.titleLarge),
-          const Spacer(),
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.settings),
@@ -100,12 +110,82 @@ class _MyExpensesState extends State<MyExpenses> {
           ),
         ],
       ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          height: 65,
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple.withOpacity(0.8),
+            borderRadius: const BorderRadius.all(Radius.circular(28)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.deepPurple.withOpacity(0.3),
+                offset: const Offset(0, 20),
+                blurRadius: 20,
+              ),
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: List.generate(
+              4,
+              (index) => Expanded(
+                child: Column(
+                  children: [
+                    AnimatedBar(isActive: selectedNavIndex == index),
+                    SizedBox(
+                      height: 36,
+                      child: Opacity(
+                        opacity: selectedNavIndex == index ? 1 : 0.5,
+                        child: IconButton(
+                          iconSize: selectedNavIndex == index ? 30 : 25,
+                          onPressed: () {
+                            setState(() {
+                              selectedNavIndex = index;
+                            });
+                          },
+                          icon: navBarIcons[index],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
       body: Column(
         children: [
           Expanded(
             child: mainContent,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 1),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: const BoxDecoration(
+        color: Color(0xFF8184FF),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
   }
